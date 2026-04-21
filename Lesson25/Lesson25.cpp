956d32deba7e5c9c6bbe942cc6dc991bed1e22d3
isaaholic <i.a.memmedli@gmail.com>
2026-04-21 11:54:10 +0400

    Lesson 25 part 2

--- Lesson25/Lesson25.cpp
+++ Lesson25/Lesson25.cpp
@@ -94,6 +94,134 @@ ostream& operator<<(ostream& os, const Fraction& f)
 }
 
 
+typedef unsigned short ushort;
+
+class Time
+{
+private:
+	ushort _hours;
+	ushort _minutes;
+	ushort _seconds;
+public:
+	Time()
+	{
+		setSeconds(0);
+		setMinute(0);
+		setHours(0);
+	}
+
+	Time(ushort seconds, ushort minutes, ushort hours)
+	{
+		setSeconds(seconds);
+		setMinute(minutes);
+		setHours(hours);
+	}
+
+	void setHours(ushort hours)
+	{
+		if (hours > 23)
+			assert(!"Hours must be between 0 and 23");
+		this->_hours = hours;
+	}
+	void setMinute(ushort minutes)
+	{
+		if (minutes > 59)
+			assert(!"Minutes must be between 0 and 59");
+		this->_minutes = minutes;
+	}
+	void setSeconds(ushort seconds)
+	{
+		if (seconds > 59)
+			assert(!"Seconds must be between 0 and 59");
+		this->_seconds = seconds;
+	}
+
+	ushort getHours() const { return _hours; }
+	ushort getMinutes() const { return _minutes; }
+	ushort getSeconds() const { return _seconds; }
+
+	// Operator overloadings
+
+	Time& operator++()
+	{
+		_seconds == 59 ? _seconds = 0 : _seconds++;
+		_seconds == 0 && _minutes == 59 ? _minutes = 0 : _minutes++;
+		_minutes == 0 && _hours == 23 ? _hours = 0 : _hours++;
+		return *this;
+	}
+
+	Time& operator--()
+	{
+		_seconds == 0 ? _seconds = 59 : _seconds--;
+		_seconds == 59 && _minutes == 0 ? _minutes = 59 : _minutes--;
+		_minutes == 59 && _hours == 0 ? _hours = 23 : _hours--;
+		return *this;
+	}
+
+	Time& operator++(int)
+	{
+		Time temp = *this;
+		_seconds == 59 ? _seconds = 0 : _seconds++;
+		_seconds == 0 && _minutes == 59 ? _minutes = 0 : _minutes++;
+		_minutes == 0 && _hours == 23 ? _hours = 0 : _hours++;
+		return temp;
+	}
+
+	Time& operator--(int)
+	{
+		Time temp = *this;
+		_seconds == 0 ? _seconds = 59 : _seconds--;
+		_seconds == 59 && _minutes == 0 ? _minutes = 59 : _minutes--;
+		_minutes == 59 && _hours == 0 ? _hours = 23 : _hours--;
+		return temp;
+	}
+
+	bool operator<(const Time& other) const
+	{
+		int totalSecondsThis = _hours * 3600 + _minutes * 60 + _seconds;
+		int totalSecondsOther = other._hours * 3600 + other._minutes * 60 + other._seconds;
+
+		return totalSecondsThis < totalSecondsOther;
+	}
+
+	bool operator>(const Time& other) const
+	{
+		return other < *this;
+	}
+
+	bool operator<=(const Time& other) const
+	{
+		return !(other < *this);
+	}
+
+	bool operator>=(const Time& other) const
+	{
+		return !(*this < other);
+	}
+
+	bool operator==(const Time& other) const
+	{
+		return _hours == other._hours && _minutes == other._minutes && _seconds == other._seconds;
+	}
+
+	bool operator!=(const Time& other) const
+	{
+		return !(*this == other);
+	}
+
+	void operator()() const
+	{
+		cout << (_hours < 10 ? "0" : "") << _hours << ":"
+			<< (_minutes < 10 ? "0" : "") << _minutes << ":"
+			<< (_seconds < 10 ? "0" : "") << _seconds << endl;
+	}
+
+	bool operator!()
+	{
+		return _hours == 0 && _minutes == 0 && _seconds == 0;
+	}
+};
+
 int main()
 {
 	//Fraction f1(1, 2);
