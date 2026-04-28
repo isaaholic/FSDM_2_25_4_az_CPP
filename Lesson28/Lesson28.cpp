6f8ed93084a5a8aef7e566d182a5bd0ad7c50607
isaaholic <i.a.memmedli@gmail.com>
2026-04-28 12:02:35 +0400

    Lesson 28

---
+++ Lesson28/Lesson28.cpp
@@ -0,0 +1,38 @@
+//#include <iostream>
+//
+//using namespace std;
+//
+//void findHipetonuz(float catetA = 0, float catetB = 0);
+//
+//int main()
+//{
+//	/*float catetA, catetB;
+//	cout << "Enter the value of catet A: ";
+//	cin >> catetA;
+//	cout << "Enter the value of catet B: ";
+//	cin >> catetB;
+//	findHipetonuz(catetA, catetB);*/
+//	findHipetonuz();
+//}
+//
+//void findHipetonuz(float catetA, float catetB) {
+//	float hipotenuz = sqrt(pow(catetA, 2) + pow(catetB, 2));
+//	cout << "The value of hipotenuz is: " << hipotenuz << endl;
+//}
+//
+
+
+// Sual - cavab
+// Nurlan - - - + 4
+// Yusuf - - + + 7
+// Medine - - + - 4
+// Senan - - - + 4
+// Ali M - - - - 2
+// Zeynal + - - - 4
+// Melek - - - - 2
+// Azer - - - - 2
+// Eltun + - - + 7
+// Fidan + - - - 4
+// Nihad - + - + 7
+// Omer - + - - 4
+// Ali I + + - + 12
