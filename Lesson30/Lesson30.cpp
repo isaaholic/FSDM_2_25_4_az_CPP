fff01e9f2a6fefcaf41882a45036f0f2e322bd1c
isaaholic <i.a.memmedli@gmail.com>
2026-05-03 12:03:04 +0400

    Lesson 30

---
+++ Lesson30/Lesson30.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    // 10x10 bir otaq duzeldirik 
+    // esas karakterimiz otagin kuncunde baslayacaq. (*#[])
+	// oxlar vasitesi ile (w,a,s,d) otaqda hereket edecek.
+    // otaqda 2 eded qutu var. Qutular random yerlerde duzulecek.
+    // qutulara toxunaraq otagin kuncune aparmaq lazimdir.
+
+    // 0 0 0 0 0
+    // 0 0 0 X 0
+    // * 0 0 * 0
+}
